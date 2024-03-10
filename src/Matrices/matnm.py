f77d31806c0eac34c85ea834ca57e82811e6067e
Mattia Ballico <mattiaballico@192.168.1.111>
2024-03-10 22:25:32 +0100

    Started creating Matrix class

--- src/Matrices/matnm.py
+++ src/Matrices/matnm.py
@@ -1,1 +1,80 @@
 # class for n*m matrices
+import math
+from typing import Tuple
+
+
+class Matrix:
+    def __init__(self, *coords: Tuple[float]) -> None:
+        Matrix.__check_len(coords)
+        self.coords: Tuple[float] = tuple(coords)
+        pass
+
+    def __str__():
+        pass
+
+    def __repr__():
+        pass
+
+    def __add__():
+        pass
+
+    def __neg__():
+        pass
+
+    def __sub__():
+        pass
+
+    def __mul__():
+        pass
+
+    def __rmul__():
+        pass
+
+    def __matmul__():
+        pass
+
+    def __truediv__():
+        pass
+
+    def __pow__():
+        pass
+
+    def to_int():
+        pass
+
+    def to_float():
+        pass
+
+    def is_zero():
+        pass
+
+    @classmethod
+    def orlata(a, b):  # search the name in english
+        pass
+
+    @staticmethod
+    def size(matrix: object):
+        pass
+
+    @staticmethod
+    def have_same_size():
+        pass
+
+    @staticmethod
+    def __check_len(coords):
+        if len(coords) == 0:
+            raise TypeError("Matrices should have at least one row")
+        n_elements = len(coords[0])
+        for row in coords:
+            if len_row := len(row) == 0:
+                raise TypeError("Each row should have at least one element in a matrix")
+            for value in len_row:
+                if isinstance(value, int | float):
+                    continue
+                raise TypeError("Matrices values can only be integers or floats")
+
+            if n_elements == len_row:
+                continue
+            raise TypeError(
+                "Matrices should have the same number of elements in each row"
+            )
